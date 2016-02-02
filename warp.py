3b4977f0ba0c59236e9e19663e566432bcc09e6e
root <root@localhost.localdomain>
2016-02-02 16:29:22 -0500

    Added arguments for easier threading

--- warp.py
+++ warp.py
@@ -10,8 +10,18 @@ import sys
 
 from passlib.utils.pbkdf2 import pbkdf2
 
+#ARGS
+parser = argparse.ArgumentParser(description="warpwallet cracker.")
+#Cusstom outfile
+parser.add_argument('-a', '--address', dest='addr', help='bitcoin address to brute', required = True)
+parser.add_argument('-t', '--threads', dest='threads', help='number of threads', required = True)
+parser.add_argument('-p', '--pass', dest='passLength', help='length of passhrase', required = True)
+#parse
+args = parser.parse_args()
+
 keyspace = string.ascii_letters + string.digits
-_pub_key = sys.argv[1].strip()
+_pub_key = (args.addr).strip()
+
 
 def xor(s1, s2):
     return "".join(chr(ord(a) ^ ord(b)) for a, b in zip(s1, s2))
@@ -50,6 +60,6 @@ def when(passphrase, *futures):
         future.add_done_callback(foo)
 
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    for p in itertools.imap(lambda t: "".join(t), itertools.product(keyspace, repeat=3)):
+with concurrent.futures.ProcessPoolExecutor(max_workers=int(args.threads)) as executor:
+    for p in itertools.imap(lambda t: "".join(t), itertools.product(keyspace, repeat=int(args.passLength))):
         when(p, executor.submit(_scrypt, p), executor.submit(_pbkdf2, p))
